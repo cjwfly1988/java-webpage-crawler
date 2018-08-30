4e6c257dedbde4bb66143e62c41fd8cc42e7ad1b
Jiawen Chen <jiawen.j.chen@student.jyu.fi>
2018-08-30 21:30:39 +0200

    remove empty lines

--- src/main/java/DomainOccurrenceHelper.java
+++ src/main/java/DomainOccurrenceHelper.java
@@ -3,7 +3,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 class DomainOccurrenceHelper {
-
     Map<String, Long> countDomainOccurrence(final List<String> domainList) {
         return domainList
                 .stream()
